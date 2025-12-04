b22ca854e7693cd754251fbfe6da12218059449c
Rennan Mendes Serenza <rennan.serenza@sanesul.ms.gov.br>
2025-12-04 11:09:07 -0400

    Update project files and clean up duplicates

--- SweetAlert2.Blazor/README.md
+++
@@ -1,2 +0,0 @@
-# SweetAlert2.Blazor
-Repositório para o desenvolvimento do nuget package para trabalhar com sweetAlert via JSInterop com Blazor.
--- wwwroot/sweetalert-wrapper.js
+++ wwwroot/sweetalert-wrapper.js
@@ -1,3 +1,14 @@
 export function sweetAlert(options) {
-    return Swal.fire(options);
+    return Swal.fire({
+        title: options.titulo,
+        text: options.texto,
+        icon: options.icone,
+        showConfirmButton: options.mostrarBotaoConfirmar,
+        showCancelButton: options.mostrarBotaoCancelar,
+        showCloseButton: options.mostrarBotaoFechar,
+        confirmButtonText: options.textoDoBotaoConfirmar,
+        cancelButtonText: options.textoDoBotaoCancelar,
+        footer: options.rodape,
+        allowOutsideClick: options.permitirClicarFora,
+    });
 }
